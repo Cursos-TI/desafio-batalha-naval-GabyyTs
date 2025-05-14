e4d434ab954c70fb40ef618d6703b76d7ab31348
GabyyTs <gabyts0819@gmail.com>
2025-05-14 16:49:15 +0000

    nivel novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,31 +10,43 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //TABULEIRO 10X10
+    int tabuleiro[10][10];
+    int i, j;
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    //NAVIO NA HORIZONTAL 
+    tabuleiro[3][1] = 3;
+    tabuleiro[3][2] = 3;
+    tabuleiro[3][3] = 3;
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    //NAVIO NA VERTICAL 
+    tabuleiro[5][6] = 3;
+    tabuleiro[6][6] = 3;
+    tabuleiro[7][6] = 3;
+
+    //EXIBIÇÃO DO TABULEIRO COM OS NAVIOS HORIZONTAL E VERTICAL
+    printf("\n*** BATALHA NAVAL ***\n");
+
+    //COLUNA COM LETRAS (A - J)
+    printf("  ");
+    for (int coluna = 0; coluna < 10; coluna++) {
+        printf("%c ", 'A' + coluna); 
+    }
+    printf("\n");
+
+    for (i = 0; i < 10; i++) {
+        printf("%d ", 1 + i); //LINHAS COM NÚMEROS (1 - 10)
+        for (j = 0; j < 10; j++){
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
